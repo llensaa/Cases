407716a2a0e26088e82effa2eeb018deec7b3f0b
Nikitosishe <billshipher801@gmail.com>
2025-03-12 14:59:42 +0700

    Update main.py

--- Case_2/main.py
+++ Case_2/main.py
@@ -12,21 +12,21 @@ def main():
     
     people = int(input(ru.PEOPLE_NUM))
     length = int(input(ru.MISSION_LENGTH))
-    water_per_person = 2.2
-    oxygen_per_person = 0.84
-    food_per_person = 1.6
-    electricity_per_person = 5
-    safety_reserve = 0.1
+    WATER_PER_PERSON = 2.2
+    OXYGEN_PER_PERSON = 0.84
+    FOOD_PER_PERSON = 1.6
+    ELECTRICITY_PER_PERSON = 5
+    SAFETY_RESERVE = 0.1
 
-    water_quantity = water_per_person * people * length
-    oxygen_quantity = oxygen_per_person * people * length
-    food_quantity = food_per_person * people * length
-    electricity_quantity = electricity_per_person * 24 * people * length
+    water_quantity = WATER_PER_PERSON * people * length
+    oxygen_quantity = OXYGEN_PER_PERSON * people * length
+    food_quantity = FOOD_PER_PERSON * people * length
+    electricity_quantity = ELECTRICITY_PER_PERSON * 24 * people * length
 
-    total_water = water_quantity * (1 + safety_reserve)
-    total_oxygen = oxygen_quantity * (1 + safety_reserve)
-    total_food = food_quantity * (1 + safety_reserve)
-    total_energy = electricity_quantity * (1 + safety_reserve)
+    total_water = water_quantity * (1 + SAFETY_RESERVE)
+    total_oxygen = oxygen_quantity * (1 + SAFETY_RESERVE)
+    total_food = food_quantity * (1 + SAFETY_RESERVE)
+    total_energy = electricity_quantity * (1 + SAFETY_RESERVE)
 
     print(f'{ru.WATER_REQUIRED} - {"{:.1f}".format(water_quantity)} {ru.LITRES}')
     print(f'{ru.OXYGEN_REQUIRED} - {"{:.1f}".format(oxygen_quantity)} {ru.KG}')
