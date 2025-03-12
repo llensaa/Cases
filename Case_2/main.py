8201de4b68518669a6dff6f24c3a88c99ccfe92b
llensaa <Lisazaza186L@gmail.com>
2025-03-12 13:03:30 +0700

    Update main.py

--- Case_2/main.py
+++ Case_2/main.py
@@ -22,10 +22,10 @@ def main():
     food_quantity = food_per_person * people * length
     electricity_quantity = electricity_per_person * 24 * people * length
 
-    print(f'{ru.WATER_REQUIRED} - {water_quantity} {ru.LITRES}')
-    print(f'{ru.OXYGEN_REQUIRED} - {oxygen_quantity} {ru.KG}')
-    print(f'{ru.FOOD_REQUIRED} - {food_quantity} {ru.KG}')
-    print(f'{ru.ELECTRICITY_REQUIRED} - {electricity_quantity} {ru.KILOWATT}')
+    print(f'{ru.WATER_REQUIRED} - {"{:.1f}".format(water_quantity)} {ru.LITRES}')
+    print(f'{ru.OXYGEN_REQUIRED} - {"{:.1f}".format(oxygen_quantity)} {ru.KG}')
+    print(f'{ru.FOOD_REQUIRED} - {"{:.1f}".format(food_quantity)} {ru.KG}')
+    print(f'{ru.ELECTRICITY_REQUIRED} - {"{:.1f}".format(electricity_quantity)} {ru.KILOWATT}')
 
 
 if __name__ == '__main__':
