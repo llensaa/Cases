d83d122f161f41e5b3d5b6bba6fa40cb63132ed4
Nikitosishe <billshipher801@gmail.com>
2025-03-30 12:11:40 +0700

    Update Random events.py

--- Case 3/Random events.py
+++ Case 3/Random events.py
@@ -41,4 +41,37 @@ match event:
         objst.people -= 2 * number_people
         objst.military -= number_military
     case 10:
-        
+        print('Отряд военных дезертировал и забрал с собой оружие!')
+        objst.people -= 2 * number_people
+        objst.military -= 3 * number_military
+    case 11:
+        print('На станции началась эпидемия!')
+        objst.people -= 4 * number_people
+        objst.military -= number_military
+    case 12:
+        print('Этот год неурожайный, провианта на всех не хватает!')
+        objst.people -= 8 * number_people
+        objst.military -= 4 * number_military
+    case 13:
+        print('Вы нашли ранее неизвестную комнату'
+              ' на станции, в которой оказались большие запасы ресурсов!')
+        objst.resources += 100
+        objst.military += 5 * number_military
+    case 14:
+        print('Ваша радиостанция уловила странный сигнал!'
+              ' Значит ли это, что не только Вы остались в живых?...')
+    case 15:
+        print('Отряд военных отправился на поиски ресурсов'
+              ' в тоннель, однако, спустя 3 дня, так и не вернулись.')
+        objst.people -= 10
+        objst.military -= 5 * number_military
+    case 16:
+        print('На вашу станцию пришли люди с оружием, но'
+              ' злых намерений у них нет, им просто нужно убежище.')
+        objst.people += 4 * number_people
+        objst.military += 5 * number_military
+    case 17:
+        print('Уже вторую неделю из тоннелей раздается плач'
+              ' ребенка. Все, кто пытался узнать, что является'
+              ' источником звука, так и не вернулись на станцию.')
+        objst.people -= 2 * number_people
