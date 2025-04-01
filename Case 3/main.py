18281ad41101e9f06b9a3fe956ba88787ece1ba5
dattilaa <alexander.dak@gmail.com>
2025-04-01 16:41:24 +0700

    Update main.py

--- Case 3/main.py
+++ Case 3/main.py
@@ -6,49 +6,76 @@ import math as mt
 import station as st
 import random_events as rdev
 
+
 def main():
-    st_1 = st.Station('Речной вокзал')
-    st_2 = st.Station('Золотая нива')
-    st_3 = st.Station('Заельцовская')
+    metro_stations = ['Площадь Карла Маркса', 'Студенческая', 'Речной вокзал',
+                      'Октябрьская', 'Площаль Ленина', 'Красный проспект',
+                      'Гагаринская', 'Заельцовская', 'Площадь Гарина-Михайловского',
+                      'Сибирская', 'Маршала Покрышкина', 'Березовая роща', 'Золотая нива']
+    names = []
+    for k in range(3):
+        print(f'{k + 1}-ая команда, выберите номер станции, за которую хотите играть:\n')
+        for n in range(len(metro_stations)):
+            print(f'{n + 1}. {metro_stations[n]}')
+        while True:
+            try:
+                index = int(input())
+                if 1 <= index <= len(metro_stations):
+                    name = metro_stations[index - 1]
+                    names.append(name)
+                    metro_stations.pop(index - 1)
+                    break
+                else:
+                    print('Такой станции нет, попробуйте снова.')
+            except ValueError:
+                print('Введите индекс станции(число).')
+    st_1 = st.Station(names[0])
+    st_2 = st.Station(names[1])
+    st_3 = st.Station(names[2])
+    enemy = st.Station('')
     names = [st_1.name, st_2.name, st_3.name]
     stations = {st_1.name: st_1, st_2.name: st_2, st_3.name: st_3}
+    actions_names = ['Вылазка', 'Прокачка', 'Битва']
+    actions = {'Вылазка': rdev.objst.searching, 'Прокачка': rdev.objst.upgrading, 'Битва': rdev.objst.battle}
+    turn_counter = 0
 
     for i in range(1, 7):
+        turn_counter += 1
         print('\nВведите индекс станции, которая собирается сделать ход: ')
         for i in range(3):
             print(f'{i + 1}. {names[i]}')
         while True:
             try:
                 st_index_turn = int(input())
-                match st_index_turn:
-                    case 1:
-                        rdev.objst = st_1
-                        rdev.objst.display_info()
-                        break
-                    case 2:
-                        rdev.objst = st_2
-                        rdev.objst.display_info()
-                        break
-                    case 3:
-                        rdev.objst = st_3
-                        rdev.objst.display_info()
-                        break
-                    case _:
-                        print("Такой станции нет, попробуйте снова.")
+                if st_index_turn in [1, 2, 3]:
+                    rdev.objst = stations[names[st_index_turn - 1]]
+                    break
+                else:
+                    print("Такой станции нет, попробуйте снова.")
             except ValueError:
                 print("Введите индекс станции(число).")
         print('\nЧто вы хотите сделать?')
-        print('1. Вылазка', '2. Прокачка станции', '3. Битва')
+        if turn_counter == 1:
+            lim = 2
+        else:
+            lim = 3
+
+        for i in range(lim):
+            print(f'{i + 1}. {actions_names[i]}')
         while True:
             try:
                 turn_choice = int(input())
                 match turn_choice:
                     case 1:
-                        rdev.objst.searching()
+                        actions.get(actions_names[0])()
                         rdev.random_choice_local()
+                        input()
+                        break
                     case 2:
-                        rdev.objst.upgrading()
+                        actions.get(actions_names[1])()
                         rdev.random_choice_local()
+                        input()
+                        break
                     case 3:
                         print('\nВведите индекс станции, на которую хотите напасть:')
                         enemies = [n for n in names if n != rdev.objst.name]
@@ -60,17 +87,17 @@ def main():
                                 enemy = stations.get(enemies[0])
                             case 2:
                                 enemy = stations.get(enemies[1])
-                        names.append(rdev.objst.name)
                         rdev.random_choice_battle()
                         input()
                         rdev.objst.battle(enemy)
                         rdev.objst.display_info()
                         enemy.display_info()
+                        break
                     case _:
                         print('Такого действия нет, попробуйте снова.')
             except ValueError:
                 print('Введите индекс действия(число).')
-        
+
     result_list = [st_1.result, st_2.result, st_3.result]
     result_list.sort()
 
