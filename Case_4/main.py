f17d7d1904de8d8bf69c5401f2f3b237c981502f
llensaa <Lisazaza186L@gmail.com>
2025-09-09 13:38:32 +0700

    textblob

--- Case_4/main.py
+++ Case_4/main.py
@@ -1,1 +1,15 @@
+from textblob import TextBlob
+from langdetect import detect
+from deep_translator import GoogleTranslator
 
+text = input()
+language = detect(text)
+if language != 'en':
+    text = GoogleTranslator(source='auto', target='en').translate(text)
+
+blob = TextBlob(text)
+
+polarity, subjectivity = blob.sentiment
+
+print(f"Polarity: {polarity}")
+print(f"Subjectivity: {subjectivity}")
