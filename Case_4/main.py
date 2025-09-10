5423d73d39084a64f4704a80e36de6187ca4f427
Eggoorre <keorg25664@gmail.com>
2025-09-10 08:39:51 +0700

    Update main.py

--- Case_4/main.py
+++ Case_4/main.py
@@ -2,6 +2,40 @@ from textblob import TextBlob
 from langdetect import detect
 from deep_translator import GoogleTranslator
 
+
+def total_sen(a):
+    counter = 0
+    for i in range(len(a)):
+        if (a[i] + a[i - 1] == '?!') or (a[i] + a[i - 1] == '!?') and i > 1:
+            counter += 1
+        elif a[i] in ['.', '!', '?']:
+            counter += 1
+    return counter
+
+
+def total_words(a):
+    return len(a.strip())
+
+
+def asw(a):
+    b = total_words(a)
+    total_syllables = len([x for x in a if x.lower in ['a', 'y',
+                                                       'u', 'e',
+                                                       'o', 'i',
+                                                       'а', 'е',
+                                                       'и', 'о',
+                                                       'я', 'ы',
+                                                       'э', 'у'
+                                                            'ё', 'ю']])
+    return total_syllables // b
+
+
+def asl(a):
+    b = total_words(a)
+    c = total_sen(a)
+    return b // c
+
+
 text = input()
 language = detect(text)
 if language != 'en':
