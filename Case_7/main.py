44ab45912a52d986a3c87600ec79c0e4eb84098c
dattilaa <alexander.dak@gmail.com>
2025-10-22 08:58:10 +0700

    Update main.py

--- Case_7/main.py
+++ Case_7/main.py
@@ -4,6 +4,10 @@ import ru_local as ru
 
 
 def get_num_hexagons() -> int:
+    '''
+    function, returning number of hexagons in row user entered
+    :return: num
+    '''
     while True:
         try:
             num = int(input(ru.ENTER_HEXAGONS))
@@ -16,11 +20,15 @@ def get_num_hexagons() -> int:
 
 
 def color_choice() -> dict:
+    '''
+    function, returning dict with colors user chose
+    :return: chosen_colors
+    '''
     colors = {'Белый': 'white', 'Красный': 'red', 'Фиолетовый': 'purple',
               'Жёлтый': 'yellow', 'Чёрный': 'black', 'Оранжевый': 'orange',
               'Синий': 'blue', 'Зелёный': 'green', 'Розовый': 'pink',
               'Коричневый': 'brown', 'Серый': 'gray', 'Голубой': 'cyan'}
-    
+
     while True:
         choice = input(ru.CHOOSE_MODE)
         if choice == '1':
@@ -28,13 +36,13 @@ def color_choice() -> dict:
             chosen_colors = {}
             for color_name in color_names:
                 chosen_colors[color_name] = colors[color_name]
-            
+
             print(f'{ru.RANDOM_CHOSEN} {", ".join(color_names)}')
             return chosen_colors
-            
+
         elif choice == '2':
             chosen_colors = {}
-            
+
             for i in range(2):
                 print('_' * 50)
                 print('\n' + ru.AVAILABLE_COLORS + '\n')
@@ -56,13 +64,22 @@ def color_choice() -> dict:
                         del colors[chosen_color]
                         break
                     print(f'\'{chosen_color}\' {ru.INVALID_VALUE}')
-            
+
             return chosen_colors
-            
+
         else:
             print(ru.ENTER_1_OR_2)
 
+
 def draw_hexagon(x, y, side_len, color):
+    '''
+    function, drawing hexagon
+    :param x: x coordinate
+    :param y: y coordinate
+    :param side_len: length of hexagons side
+    :param color: hexagons fillcolor
+    :return: None
+    '''
     trt.tracer(0)
     trt.right(90)
     trt.fillcolor(color)
@@ -76,10 +93,15 @@ def draw_hexagon(x, y, side_len, color):
     trt.penup()
     trt.home()
 
+
 def draw_tesselation():
+    '''
+    function, drawing tesselation
+    :return: None
+    '''
     colors = color_choice()
     used_colors = colors.values()
-    
+
     colors_list = [a for a in used_colors]
     color1, color2 = colors_list[0], colors_list[1]
 
