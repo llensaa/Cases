b82670f8661ceb6e2fb27008b1f9dc33a5b46523
dattilaa <alexander.dak@gmail.com>
2025-11-05 00:16:24 +0700

    Update ru_local.py

--- Case_8/ru_local.py
+++ Case_8/ru_local.py
@@ -1,1 +1,77 @@
+# ERRORS
+FOUND_INCORRECT_DATE = 'Обнаружена строка с некорректной датой'
+FOUND_INCORRECT_SUM = 'Обнаружена строка с некорректной суммой
+LOST_INCORRECT_DATA_EL = 'Пропущен элемент с некорректной датой
+DIVERSED_OR_UNKNOWN = 'Файл содержит смешанные или неизвестные типы данных!
 
+# CATEGORIES
+FOOD = 'Еда'
+TRANSPORT = 'Транспорт'
+AMUSEMENT = 'Развлечения'
+HEALTH = 'Здоровье'
+HOME = 'Дом'
+SPORT = 'Спорт'
+OTHER = 'Другое'
+
+# SUBCATEGORIES
+GORODOK = 'городок'
+X5 = 'пятёрочка'
+YARCHE = 'ярче'
+SAMOKAT = 'самокат'
+PRODUCTS = 'продукты'
+DINNER = 'обед'
+SUSHI = 'суши'
+BUS = 'автобус'
+TAXI = 'такси'
+PETROL = 'бензин'
+SCOOTER = 'самокат'
+RESTAURANT = 'ресторан'
+THEATRE = 'театр'
+CINEMA = 'кино'
+CONCERT = 'концерт'
+TOUR = 'тур'
+RECREATION = 'отдых'
+DOCTOR = 'врач'
+DRUGS = 'лекарства'
+HOSPITAL = 'больница'
+EYES = 'глаза'
+DENTIST = 'стоматолог'
+PHARMACY = 'аптека'
+UTILITIES = 'коммунальные услуги'
+RENT = 'аренда квартиры'
+FURNITURE = 'мебель'
+FITNESS = 'фитнес'
+YOGA = 'йога'
+POOL= 'бассейн'
+GYM = 'зал'
+TENNIS = 'теннис'
+MASSAGE = 'массаж'
+
+# FINANCIAL STATISTICS
+INCOME = 'Доход'
+INCOMES = 'Доходы'
+EXPENSE = 'Расход'
+EXPENSES = 'Расходы'
+BALANCE = 'Баланс'
+AMOUNT = 'Количество операций'
+COSTS = 'Затраты'
+COSTS_PART = 'Доля в общих расходах
+
+# MONTHS
+JANUARY = 'Январь'
+FEBUARY = 'Февраль'
+MARCH = 'Март'
+APRIL = 'Апрель'
+MAY = 'Май'
+JUNE = 'Июнь'
+JULE = 'Июль'
+AUGUST = 'Август'
+SEPTEMBER = 'Сентябрь'
+OCTOBER = 'Октябрь'
+NOVEMBER = 'Ноябрь'
+DECEMBER = 'Декабрь'
+
+# REPORT
+FINANCIAL_REPORT = '===ФИНАНСОВЫЙ ОТЧЁТ===\n'
+MAIN_RATIOS = 'ОСНОВНЫЕ ПОКАЗАТЕЛИ:'
+MONTH_STATS = '\n===СТАТИСТИКА ПО МЕСЯЦАМ===\n'
