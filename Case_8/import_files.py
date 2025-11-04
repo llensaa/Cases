0e6b88b235e2ce36f80970fffc60588ac2fce324
llensaa <Lisazaza186L@gmail.com>
2025-11-05 01:21:09 +0700

    Update import_files.py

--- Case_8/import_files.py
+++ Case_8/import_files.py
@@ -4,6 +4,12 @@ import ru_local as ru
 
 
 def is_valid_date(date_str: str, date_format: str = '%Y-%m-%d') -> bool:
+    '''
+    function, checking wheteher date is correct or not
+    :param: date_str
+    :param: date_format
+    :return: True or False
+    '''
     try:
         datetime.strptime(date_str, date_format)
         return True
@@ -11,18 +17,33 @@ def is_valid_date(date_str: str, date_format: str = '%Y-%m-%d') -> bool:
         return False
 
 def read_csv_file(filename: str) -> list:
+    '''
+    function, reading csv file with recordings
+    :param: filename
+    :return: csv_list
+    '''
     with open(filename, 'r', encoding='utf-8') as f:
         csv_list = [line.strip().split(',') for line in f]
     return csv_list
 
 
 def read_json_file(filename: str) -> list:
+    '''
+    function, reading json file with recordings
+    :param: filename
+    :return: json_list
+    '''
     with open(filename, 'r', encoding='utf-8') as f:
         json_list = json.load(f)
         return json_list
 
 
 def import_financial_data(filename: str) -> list:
+    '''
+    function, sorting file(csv or json) and creating a list of dictionaries from it
+    :param: filename
+    :return: records
+    '''
     if filename.endswith('.csv'):
         data = read_csv_file(filename)
     else:
