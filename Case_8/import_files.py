d1f5b26b064f6f2d437943236d03fbd6fdc989f6
Eggoorre <keorg25664@gmail.com>
2025-11-04 23:31:47 +0700

    Update import_files.py

--- Case_8/import_files.py
+++ Case_8/import_files.py
@@ -2,26 +2,48 @@ import json
 from datetime import datetime
 
 
-def is_valid_date(date_str: str, date_format: str = '%Y-%m-%d') -> bool:
+def is_valid_date(date_str: str, date_format: str = "%Y-%m-%d") -> bool:
+    '''
+    function, checking wheteher date is correct or not
+    :param: date_str
+    :param: date_format
+    :return: True or False
+    '''
     try:
         datetime.strptime(date_str, date_format)
         return True
     except ValueError:
         return False
 
+
 def read_csv_file(filename: str) -> list:
+    '''
+    function, reading csv file with recordings
+    :param: filename
+    :return: csv_list
+    '''
     with open(filename, 'r', encoding='utf-8') as f:
         csv_list = [line.strip().split(',') for line in f]
     return csv_list
 
 
 def read_json_file(filename: str) -> list:
+    '''
+    function, reading json file with recordings
+    :param: filename
+    :return: json_list
+    '''
     with open(filename, 'r', encoding='utf-8') as f:
         json_list = json.load(f)
         return json_list
 
 
 def import_financial_data(filename: str) -> list:
+    '''
+    function, sorting file(csv or json) and creating a list of dictionaries from it
+    :param: filename
+    :return: records
+    '''
     if filename.endswith('.csv'):
         data = read_csv_file(filename)
     else:
@@ -34,25 +56,21 @@ def import_financial_data(filename: str) -> list:
             if len(row) >= 4:
                 date_str = row[0].strip()
                 if not is_valid_date(date_str):
-                    print(f'Пропущена строка с некорректной датой: {date_str}')
+                    print(f'Обнаружена строка с некорректной датой: {date_str}')
                     continue
 
                 try:
                     amount = float(row[1])
                 except ValueError:
-                    print(f'Пропущена строка с некорректной суммой: {row[1]}')
+                    print(f'Обнаружена строка с некорректной суммой: {row[1]}')
                     continue
 
                 record = {
                     'date': date_str,
                     'amount': amount,
                     'description': row[2].strip(),
-                    'type': ''
+                    'type': row[3].strip().lower()
                 }
-                if int(record['amount']) >= 0:
-                    record['type'] += 'Доход'
-                else:
-                    record['type'] += 'Расход'
                 records.append(record)
 
     elif all(isinstance(row, dict) for row in data):
@@ -65,6 +83,4 @@ def import_financial_data(filename: str) -> list:
     else:
         print(f'Файл содержит смешанные или неизвестные типы данных!')
 
-    return sorted(records, key=lambda x: x['date'])
-
-
+    return records
