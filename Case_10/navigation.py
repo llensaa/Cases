a7745182b31b9507cb4579d62ff16e2bf160da62
dattilaa <alexander.dak@gmail.com>
2026-01-31 15:23:32 +0700

    Update navigation.py

--- Case_10/navigation.py
+++ Case_10/navigation.py
@@ -1,1 +1,54 @@
+import os
+from datetime import datetime
+from typing import List, Dict, Any, Tuple
+import utils  # Импорт функций архитектора
 
+def get_current_drive() -> str:
+    """Получение текущего диска Windows"""
+    # TODO: Вернуть текущий диск (например: "C:")
+    # Использовать os.path.splitdrive()
+    pass
+
+def list_available_drives() -> List[str]:
+    """Получение списка доступных дисков Windows"""
+    # TODO: Вернуть список доступных дисков (['C:', 'D:', ...])
+    # Использовать os.listdir('/') не подойдет для Windows!
+    # Исследовать: использовать win32api или другие методы
+    pass
+
+def list_directory(path: str) -> Tuple[bool, List[Dict[str, Any]]]:
+    """Отображение содержимого каталога в Windows"""
+    # TODO: Используя utils.safe_windows_listdir(), получить содержимое
+    # Для каждого элемента вернуть словарь с информацией:
+    # {'name': 'file.txt', 'type': 'file', 'size': 1024, 'modified': '2024-01-15', 'hidden': False}
+    # Использовать utils.is_hidden_windows_file() для проверки скрытых файлов
+    # Вернуть (True, данные) при успехе, (False, []) при ошибке
+    pass
+
+def format_directory_output(items: List[Dict[str, Any]]) -> None:
+    """Форматированный вывод содержимого каталога для Windows"""
+    # TODO: Красиво отформатировать вывод используя данные из list_directory()
+    # Учесть что в Windows есть системные и скрытые файлы
+    # Показать диски если находимся в корне
+    pass
+
+def move_up(current_path: str) -> str:
+    """Переход в родительский каталог в Windows"""
+    # TODO: Использовать utils.get_parent_path() для получения родителя
+    # Проверить валидность нового пути через utils.validate_windows_path()
+    # Учесть переход между дисками
+    pass
+
+def move_down(current_path: str, target_dir: str) -> Tuple[bool, str]:
+    """Переход в указанный подкаталог в Windows"""
+    # TODO: Проверить что target_dir существует через utils.safe_windows_listdir()
+    # Сформировать новый путь и проверить через utils.validate_windows_path()
+    # Вернуть (True, новый_путь) при успехе, (False, текущий_путь) при ошибке
+    pass
+
+def get_windows_special_folders() -> Dict[str, str]:
+    """Получение путей к специальным папкам Windows"""
+    # TODO: Вернуть словарь с путями к папкам:
+    # {'Desktop': 'C:\\Users\\...', 'Documents': '...', 'Downloads': '...'}
+    # Использовать os.environ для получения USERPROFILE и других переменных
+    pass
