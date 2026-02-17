b02511621c21c4c208659a3dcf94bcc43172fffe
llensaa <Lisazaza186L@gmail.com>
2026-02-18 00:51:29 +0700

    Add files via upload

---
+++ Case_11/ru_local.py
@@ -0,0 +1,44 @@
+station_labels = {
+    'station': "Автомат",
+    'max_queue': "максимальная очередь",
+    'fuel_types': "Марки бензина"
+}
+
+car_keys = {
+    'oil_type': 'oil type',
+    'fuel_amount': 'fuel amount',
+    'time': 'time',
+    'num': 'num',
+    'end_time': 'end_time'
+}
+
+messages = {
+    'new_client': "В {time} новый клиент:",
+    'client_added': "встал в очередь к автомату",
+    'client_unserved': "не смог заправить автомобиль и покинул АЗС",
+    'client_departed': "клиент завершил заправку и покинул АЗС",
+    'file_read_error': "Ошибка чтения файла",
+    'invalid_format': "Неверный формат данных в файле"
+}
+
+time_labels = {
+    'minutes': "минут",
+    'hours': "часов",
+    'start_time': "время начала",
+    'end_time': "время окончания",
+    'duration': "длительность"
+}
+
+time_messages = {
+    'invalid_format': "Неверный формат времени, ожидается ЧЧ:ММ",
+    'exceeds_day': "Время превышает 23:59"
+}
+
+daily_stats = {
+    'title': "ДНЕВНАЯ СТАТИСТИКА",
+    'sold': "Продано бензина",
+    'liters': "литров",
+    'total_revenue': "Общая сумма продаж",
+    'lost_revenue': "Упущенная выгода",
+    'currency': "руб"
+}
