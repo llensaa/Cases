cb82090f4a588c1667e89f5916aabb8f792a518d
dattilaa <alexander.dak@gmail.com>
2026-02-17 22:45:59 +0700

    Update main.py

--- Case_11/main.py
+++ Case_11/main.py
@@ -1,53 +1,58 @@
-import some_functions as sf
-import economic_analysis as ef
+import file_operations as fo
+import station_management as sm
+import time_utils as tu
+import economic_analysis as ec
 
 
-def main():
-    gas_st_info = sf.gas_st_read('gas_stations.txt')
-    cars_info = sf.input_read('input_file.txt')
-    petrol_info = ef.petrol_stat()
+def main() -> None:
+    """Основная логика симуляции работы АЗС"""
+    stations = fo.read_gas_stations('gas_stations.txt')
+    cars = fo.read_cars_input('input_file.txt')
 
-    for car in cars_info:
-        car['fuel_end_time'] = ef.count_time(car)
-        car['fuel_end_minutes'] = ef.transform_to_minutes(car['fuel_end_time'])
+    queues = sm.create_station_queues(stations)
+    fuel_stats = ec.create_fuel_statistics()
+    total_revenue = 0
+    lost_revenue = 0
 
-    lost_rev = 0
-    total_rev = 0
-
-    gas_st_queue = {station['num']: [] for station in gas_st_info}
+    for car in cars:
+        car['end_time'] = tu.calculate_end_time(car['time'], car['fuel amount'])
+        car['end_minutes'] = tu.time_to_minutes(car['end_time'])
 
     for minute in range(1440):
-        current_time = f'{minute // 60:02d}:{minute % 60:02d}'
+        current_time = tu.minutes_to_time(minute)
 
-        current_cars = [car for car in cars_info if car['time'] == current_time]
-        for car in current_cars:
-            best_st = sf.queue_add(car, gas_st_info, gas_st_queue)
+        arriving_cars = [car for car in cars if car['time'] == current_time]
+        for car in arriving_cars:
+            best_station = sm.find_best_station(car, stations, queues)
 
-            if not best_st:
-                lost_rev += ef.revenue(car)
-                print(f'Клиент {car['time']} {car['oil type']} {car['fuel amount']} '
+            if not best_station:
+                lost_revenue += ec.calculate_revenue(car)
+                print(f'Клиент {car["time"]} {car["oil type"]} {car["fuel amount"]} '
                       f'не смог заправиться и покинул АЗС')
                 continue
 
-            car_in_queue = car.copy()
-            car_in_queue['end_time'] = car['fuel_end_time']
-            gas_st_queue[best_st].append(car_in_queue)
+            sm.add_car_to_queue(car, best_station, queues, car['end_time'])
+            print(f'Клиент {car["time"]} {car["oil type"]} {car["fuel amount"]}'
+                  f' встал в очередь к автомату {best_station}')
 
-            print(f'Клиент {car['time']} {car['oil type']} {car['fuel amount']}'
-                  f' встал в очередь к автомату {best_st}')
-            petrol_info[car['oil type']] += car['fuel amount']
-            total_rev += ef.revenue(car)
+            ec.update_fuel_statistics(fuel_stats, car)
+            total_revenue += ec.calculate_revenue(car)
 
-        for st_num, queue in gas_st_queue.items():
-            leaving_cars = [car for car in queue if car['end_time'] == current_time]
-            for car in leaving_cars:
-                queue.remove(car)
-                print(
-                    f'В {car['end_time']} клиент {car['time']} {car['oil type']}'
-                    f' {car['fuel amount']} заправил свой автомобиль и покинул АЗС')
+        departed_cars = sm.remove_departed_cars(queues, current_time)
+        for car in departed_cars:
+            print(f'В {car["end_time"]} клиент {car["time"]} {car["oil type"]} '
+                  f'{car["fuel amount"]} заправил свой автомобиль и покинул АЗС')
 
-    ef.print_stat(petrol_info, total_rev, lost_rev)
+    ec.print_daily_statistics(fuel_stats, total_revenue, lost_revenue)
 
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
+
+
