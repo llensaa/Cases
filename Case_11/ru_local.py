8cc00ff886eda7b3b711c9251f5183d1c646b70e
llensaa <Lisazaza186L@gmail.com>
2026-02-18 08:25:30 +0700

    Update ru_local.py

--- Case_11/ru_local.py
+++ Case_11/ru_local.py
@@ -1,7 +1,7 @@
 station_labels = {
-    'station': "Автомат",
-    'max_queue': "максимальная очередь",
-    'fuel_types': "Марки бензина"
+    'station': 'Автомат',
+    'max_queue': 'максимальная очередь',
+    'fuel_types': 'Марки бензина'
 }
 
 car_keys = {
@@ -13,32 +13,32 @@ car_keys = {
 }
 
 messages = {
-    'new_client': "В {time} новый клиент:",
-    'client_added': "встал в очередь к автомату",
-    'client_unserved': "не смог заправить автомобиль и покинул АЗС",
-    'client_departed': "клиент завершил заправку и покинул АЗС",
-    'file_read_error': "Ошибка чтения файла",
-    'invalid_format': "Неверный формат данных в файле"
+    'new_client': 'В',
+    'client_added': 'встал в очередь к автомату',
+    'client_unserved': 'не смог заправить автомобиль и покинул АЗС',
+    'client_departed': 'Клиент завершил заправку и покинул АЗС',
+    'file_read_error': 'Ошибка чтения файла',
+    'invalid_format': 'Неверный формат данных в файле'
 }
 
 time_labels = {
-    'minutes': "минут",
-    'hours': "часов",
-    'start_time': "время начала",
-    'end_time': "время окончания",
-    'duration': "длительность"
+    'minutes': 'минут',
+    'hours': 'часов',
+    'start_time': 'время начала',
+    'end_time': 'время окончания',
+    'duration': 'длительность'
 }
 
 time_messages = {
-    'invalid_format': "Неверный формат времени, ожидается ЧЧ:ММ",
-    'exceeds_day': "Время превышает 23:59"
+    'invalid_format': 'Неверный формат времени, ожидается ЧЧ:ММ',
+    'exceeds_day': 'Время превышает 23:59'
 }
 
 daily_stats = {
-    'title': "ДНЕВНАЯ СТАТИСТИКА",
-    'sold': "Продано бензина",
-    'liters': "литров",
-    'total_revenue': "Общая сумма продаж",
-    'lost_revenue': "Упущенная выгода",
-    'currency': "руб"
+    'title': 'ДНЕВНАЯ СТАТИСТИКА',
+    'sold': 'Продано бензина',
+    'liters': 'литров',
+    'total_revenue': 'Общая сумма продаж',
+    'lost_revenue': 'Упущенная выгода',
+    'currency': 'руб'
 }
